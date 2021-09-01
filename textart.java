62c3ab7f57f7b9c0852eb1a05ef3f4c6affef28e
Jake-P-N <89948289+Jake-P-N@users.noreply.github.com>
2021-09-01 16:38:02 -0700

    Create textart source code

---
+++ textart.java
@@ -0,0 +1,13 @@
+// Text Art
+// @authour Jake Nellesen
+// @version September 1, 2021
+
+public class textart {
+    public static void main(String[] args) throws Exception {
+        System.out.println("        <(__)> | | |\n        | \\/ | \\_|_/\n        \\^  ^/   |\n        /\\--/\\  /|\n       /  \\/  \\/ |\n\n\n\n");
+        // This one was generated using a PNG to ACII art converter, but it does not really look like much
+        // System.out.println("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHHHHHQmPAUAXRHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHB&AqCx\":^\">|^|SBHHHH\nHHHHHHHHHHHHHHHHHHHHHHRPpS]+^`` ,\"\",`mHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHMPCt,'-`,\"_` CHHHH\nHHHHHHHHHHHHHHHHHHHHH@gai+++>|+1+\"\\` qHHHH\nHHHHHHHHHHHHHHHHHHH&$s1rt}++t+>++\"~`1MHHHH\nHHHHHHHHHHHHHHHB&AI}\"\"\"|___|\"+_\\|\"_,mHHHHH\nHHHHHHHHHHHHBOdc\">>>_^~___|^\\^|:'\"_vBHHHHH\nHHHHHHHHHBmTj>|__|^\\',,,,:\\''''-,,+@HHHHHH\nHHHHHHH@n>\\'':,```,`,```````,-```vMHHHHHHH\nHHHB&%l|'-,``` ```   ````````` +gBHHHHHHHH\nRPCt:```    ,:>+'       `   ~yPBHHHHHHHHHH\n}```,+tr3qPWRBHHMn:      ,\"CRHHHHHHHHHHHHH\n`|z%@HHHHHHHHHHHHBPy\\ +PP@HHHHHHHHHHHHHHHH\nPBHHHHHHHHHHHHHHHHHHR$aCWHHHHHHHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHRXn2vbmXXRHHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHB@A%aenCngQHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHBM&WMDHHHHHHHHHHHH\nHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH\n\n\n");
+        // A little house
+        System.out.println("A little house:\n   /\\   \n  /  \\   \n | \"\" |  \n | || |  \nHHHHHHHHHH\n");
+    }
+}
